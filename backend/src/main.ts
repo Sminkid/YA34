370ea0c6019f90862f61e032c5dee6d9b77c57c5
Sminkid <jonathan.archiechang@gmail.com>
2026-08-22 16:14:49 +1000

    fixing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 });
   await app.listen(process.env.PORT || 3001);
-  console.log('YA34 backend running on http://localhost:3001');
+  console.log(`YA34 backend running on port ${process.env.PORT || 3001}`);
 }
 bootstrap();
